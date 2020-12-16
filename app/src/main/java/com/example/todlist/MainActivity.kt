69d5e9bfe0daa79f65aed9491be435a7a759ee3a
Mosesteshome <64324115+Mosesteshome@users.noreply.github.com>
2020-12-16 19:35:29 +0300

    updated

--- app/src/main/java/com/example/todlist/MainActivity.kt
+++ app/src/main/java/com/example/todlist/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.todlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setup
+        Toast.makeText(this,"what is this", Toast.LENGTH_SHORT)
+
+
     }
 
 
